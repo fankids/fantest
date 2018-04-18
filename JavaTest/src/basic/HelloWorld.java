b4ba5930c3ffb2a34a17cdc88d478da5250ab68e
fankids <lcl080101@163.com>
2018-04-18 14:07:30 +0800

    modify by fankids@20180418

--- JavaTest/src/basic/HelloWorld.java
+++ JavaTest/src/basic/HelloWorld.java
@@ -1,5 +1,16 @@
 package basic;
 
+/**
+ * 
+ * 
+ * @ClassName: HelloWorld
+ * @Description: TODO
+ * @author: clli
+ * @date: 2018-4-18ионГ9:59:11
+ * @version:  1.0
+ * @since: JDK 1.6
+ *
+ */
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
